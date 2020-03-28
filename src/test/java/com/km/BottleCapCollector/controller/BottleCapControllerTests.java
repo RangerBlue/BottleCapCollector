9f05d7813dc29776542d1120476730961974c8cf
Kamil Machul <kamil.machul@gmail.com>
2020-03-28 20:12:56 +0100

    Add another part of tests for controller

--- src/test/java/com/km/BottleCapCollector/controller/BottleCapControllerTests.java
+++ src/test/java/com/km/BottleCapCollector/controller/BottleCapControllerTests.java
@@ -5,14 +5,19 @@ import com.km.BottleCapCollector.service.BottleCapService;
 import com.km.BottleCapCollector.service.FileStorageService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
@@ -49,7 +54,6 @@ public class BottleCapControllerTests {
     public void getBottleCaps() throws Exception {
 
         BottleCap cap = new BottleCap("cap1");
-
         List<BottleCap> allCaps = Arrays.asList(cap);
 
         given(service.getAllBottleCaps()).willReturn(allCaps);
@@ -58,5 +62,24 @@ public class BottleCapControllerTests {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
+    }
+
+    @Test
+    public void getBottleCap() throws Exception {
+        this.mvc.perform(get("/cap/1"))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void uploadMultipleFiles() throws Exception {
+        String fileName = "captest1.jpg";
+        MockMultipartFile file = new MockMultipartFile("file",fileName,
+                "text/plain", "test data".getBytes());
+
+        this.mvc.perform(MockMvcRequestBuilders.multipart("/uploadMultipleFiles")
+                .file(file)
+                .file(file)
+                .param("capName", "Beer"))
+                .andExpect(status().is(200));
     }
 }
