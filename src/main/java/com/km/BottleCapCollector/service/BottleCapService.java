d0a5f4fcc84dfc04b1e9fd5cdcee44198ae9e8c9
Kamil Machul <kamil.machul@gmail.com>
2020-11-06 00:00:42 +0100

    Clear cache when new cap is added
    (cherry picked from commit 03c36206fdc28de69e5b19d37ec4e150116d66a0)

--- src/main/java/com/km/BottleCapCollector/service/BottleCapService.java
+++ src/main/java/com/km/BottleCapCollector/service/BottleCapService.java
@@ -3,8 +3,12 @@ package com.km.BottleCapCollector.service;
 
 import com.km.BottleCapCollector.model.BottleCap;
 import com.km.BottleCapCollector.repository.BottleCapRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +19,20 @@ import java.util.Optional;
 @Service
 public class BottleCapService {
 
+    private static final Logger logger = LogManager.getLogger(BottleCapService.class);
+
     @Autowired
     private BottleCapRepository repository;
 
-    public BottleCap addBottleCap(BottleCap cap){
+    @Caching(evict =
+    @CacheEvict(value = "caps", allEntries = true))
+    public BottleCap addBottleCap(BottleCap cap) {
         return repository.save(cap);
     }
 
     @Cacheable(value = "caps")
     public List<BottleCap> getAllBottleCaps(){
+        logger.info("Entering getAllBottleCap method in service layer");
         return (List<BottleCap>)repository.findAll();
     }
 
