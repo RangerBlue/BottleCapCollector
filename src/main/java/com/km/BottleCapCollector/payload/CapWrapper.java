bd12648585b72afa3b5698b16b3dc9be4a6f0d60
Kamil Machul <kamil.machul@gmail.com>
2021-01-09 19:35:29 +0100

    Add catalog endpoint and description field
    (cherry picked from commit cfb53bbab4f1c16250b3155cf9dedd436807a518)

---
+++ src/main/java/com/km/BottleCapCollector/payload/CapWrapper.java
@@ -0,0 +1,28 @@
+package com.km.BottleCapCollector.payload;
+
+public class CapWrapper extends PictureWrapper{
+    private String name;
+    private String description;
+
+    public CapWrapper(long id, String url, String name, String description) {
+        super(id, url);
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
